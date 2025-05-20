db9cffb47221d37e66258b7c2c95110f58a1c2af
K Khryhorenko <khryhorenko03@gmail.com>
2025-05-20 19:11:29 +0000

    up

---
+++ Readme.md
@@ -0,0 +1,3 @@
+python3 manage.py collectstatic
+
+docker compose up --build -d
--- backend/hospital_website/settings.py
+++ backend/hospital_website/settings.py
@@ -65,7 +65,10 @@ ROOT_URLCONF = 'hospital_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'backend/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
